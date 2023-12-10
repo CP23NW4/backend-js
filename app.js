4dd19b1859164a4a1c5bf411ce42410ac459c065
punchsppt <supapit.r@mail.kmutt.ac.th>
2023-12-10 22:38:11 +0700

    can get all and get by id of stray animals data

--- app.js
+++ app.js
@@ -6,26 +6,149 @@
 // model.index ไว้เชื่อม db
 
 
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// // const route = require('./routes');    // api
+// const port = 8090;
+// // const model = require('./models')
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // app.use('/', route);
+// // Define a route
+// app.get('/', (request, response) => {
+//   res.send('Hello, Express! you can testå');
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Backend running with port : ${port} !!!`);
+// });
+
+// ---------------------------------------------------------
+// Example route file
+// const express = require('express');
+// // const router = express.Router();
+// const connectDB = require('./database'); // Import the database connection
+// const strayAnimalRoute = require('./routes/strayAnimalRoutes');
+
+// const app = express();
+// const port = 8090;
+// const route = require('./routes')
+
+// Connect to MongoDB
+// connectDB(); // Call the function to establish the database connection
+
+// Include routes
+// app.use('/api/strayAnimals', strayAnimalRoute);
+// app.use('/', route)
+
+
+// app.get('/', (request, response) => {
+//   res.send('Hello, Express! you can testå');
+// });
+
+// Start the server
+// const port = process.env.port || 8090
+// app.listen(port, () => {
+//   console.log(`Backend running with port : http://localhost:${port} !!!`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+// module.exports = router;
+// ----------------------------------------
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// // Initialize express app
+// const app = express();
+
+// // Connect to MongoDB
+// mongoose.connect('mongodb+srv://mnwadmin:meowandwoof@meowandwoof.gcedq3t.mongodb.net/', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch((err) => {
+//     console.error('Error connecting to MongoDB:', err.message);
+// });
+
+// // Other configurations and middleware setup
+
+// // Import and use strayAnimalRoutes
+// const strayAnimalRoutes = require('./routes/strayAnimalRoutes');
+// app.use('/', strayAnimalRoutes);
+
+// // Start the server
+// const PORT = process.env.PORT || 8090;
+// app.listen(PORT, () => {
+//     console.log(`Server started on port http://localhost:${PORT}`);
+// });
+
+
+// server.js
+
+// Import necessary modules
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-// const route = require('./routes');    // api
-const port = 8090;
-// const model = require('./models')
 
+// Create Express app
+const app = express();
+app.use(cors()) // Enable CORS for all routes
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// MongoDB connection
+mongoose.connect('mongodb+srv://mnwadmin:meowandwoof@meowandwoof.gcedq3t.mongodb.net/meowandwoof', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// app.use('/', route);
-// Define a route
-app.get('/', (request, response) => {
-  res.send('Hello, Express! you can testå');
+// Define Schema for Stray Animal
+const strayAnimalSchema = new mongoose.Schema({
+  name: String,
+  picture: String,
+  type: String,
+  gender: String,
+  color: String,
+  description: String,
+  ownerId: String,
+  createdOn: Date,
+  updatedOn: Date,
+});
+
+// Create a model based on the schema
+const StrayAnimal = mongoose.model('StrayAnimal', strayAnimalSchema, 'strayAnimals');
+
+// Define route to get all stray animals
+app.get('/api/strayAnimals', async (req, res) => {
+  try {
+    // Fetch all stray animals from MongoDB
+    const allStrayAnimals = await StrayAnimal.find();
+    res.json(allStrayAnimals);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get('/api/strayAnimals/:saId', async (req, res) => {
+  try {
+    const strayAnimalbyId = await StrayAnimal.findById(req.params.saId);
+    res.json(strayAnimalbyId);
+  } catch (err) {
+    res.status(404).json({ message: 'Stray animal not found' });
+  }
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Backend running with port : ${port} !!!`);
+const PORT = process.env.PORT || 8090;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
