4ab82d2a0f273022b314bbb3dcac88d6262f7436
Supapit R <73163610+punchsppt@users.noreply.github.com>
2023-12-15 14:53:17 +0700

    Delete routes  in root directory

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-// const express = require('express')
-// const router = express.Router()
-// const strayAnimalRoute = require('./strayAnimalRoutes')
-
-// router.use(strayAnimalRoute)
-// module.exports = router
