d8cbeb6b55d249d2dd7f5a0fbba232b1d76c1bfa
punchsppt <supapit.r@mail.kmutt.ac.th>
2023-12-30 15:07:56 +0700

    SP4-PBI11&12: Create GET, DELETE, and PUT for Users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,6 +26,53 @@ async function registerUser(req, res) {
     }
   }
 
-  module.exports = {
-    registerUser,
-  };
+  // Get all users
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+// Get user by ID
+async function getUserById(req, res) {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+// Delete user by ID - Placeholder
+async function deleteUserById(req, res) {
+  try {
+    // Implement user deletion logic here using req.params.userId
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+// Edit user by ID - Placeholder for future use (using PUT)
+async function editUserById(req, res) {
+  try {
+    // Implement user editing logic here using req.params.userId and req.body
+    res.json({ message: 'User updated' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+module.exports = {
+  registerUser,
+  getAllUsers,
+  getUserById,
+  deleteUserById,
+  editUserById,
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/userController');
 const { loginUser } = require('../controllers/userAuthController');
-const { authenticateToken } = require('../middlewares/userAuthMiddleware');
+// const { authenticateToken } = require('../middlewares/userAuthMiddleware');
 const User = require('../models/User');
 
 // GET all users (not authen)
@@ -23,27 +23,48 @@ router.post('/register', registerUser);
 // User login
 router.post('/login', loginUser);
 
-// Example protected route to fetch user profile based on user ID
-router.get('/profile', authenticateToken, async (req, res) => {
+
+// Get all users
+router.get('/', /* authenticateToken ,*/ async (req, res) => {
   try {
-    const user = await User.findById(req.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    
-    // Extract and send necessary user profile information
-    const userProfile = {
-      username: user.username,
-      email: user.email,
-      phoneNumber: user.phoneNumber,
-      // Add other necessary fields
-    };
-    
-    res.status(200).json(userProfile);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
+// Get user by ID
+router.get('/:userId', /* authenticateToken ,*/ async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Delete user by ID - Placeholder
+router.delete('/:userId', /* authenticateToken ,*/ async (req, res) => {
+  try {
+    // Implement user deletion logic here using req.params.userId
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Edit user by ID - Placeholder for future use (using PUT)
+router.put('/:userId', /* authenticateToken ,*/ async (req, res) => {
+  try {
+    // Implement user editing logic here using req.params.userId and req.body
+    res.json({ message: 'User updated' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
