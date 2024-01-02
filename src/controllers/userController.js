81f33eefc0605a00113ebd861a528d656f6a3755
punchsppt <supapit.r@mail.kmutt.ac.th>
2024-01-03 00:28:50 +0700

    SP4-PBI11: Can use EDIT user method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@
 
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 require('dotenv').config({ path: '../.env' });
 const secretKey = process.env.SECRET_KEY; // Accessing the secret key from the environment variable}
@@ -71,7 +72,9 @@ async function loginUser(req, res) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    const isPasswordValid = await user.comparePassword(password);
+
+    // Compare the provided password with the stored encrypted password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       return res.status(401).json({ message: 'Invalid password' });
@@ -135,7 +138,51 @@ async function deleteUserById(req, res) {
 // Edit user by ID - Placeholder for future use (using PUT)
 async function editUserById(req, res) {
   try {
-    // Implement user editing logic here using req.params.userId and req.body
+    const userId = req.params.userId;
+    // const { username, phoneNumber, userAddress, password } = req.body;
+    const { username, phoneNumber, userAddress } = req.body;
+
+     // Check if userId is provided
+     if (!userId) {
+      return res.status(400).json({ message: 'User ID is required' });
+    }
+
+    // Find the user by userId
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // If username is provided, ensure it's unique
+    if (username && username !== user.username) {
+      const existingUser = await User.findOne({ username });
+
+      if (existingUser) {
+        return res.status(400).json({ message: 'Username already exists' });
+      }
+
+      user.username = username;
+    }
+
+    // Update phoneNumber if provided
+    if (phoneNumber) {
+      user.phoneNumber = phoneNumber;
+    }
+
+    // Update userAddress if provided
+    if (userAddress) {
+      user.userAddress = userAddress;
+    }
+
+    // // Update password if provided (encrypt using bcrypt)
+    // if (password) {
+    //   const hashedPassword = await bcrypt.hash(password, 10);
+    //   user.password = hashedPassword;
+    // }
+
+    await user.save();
+
     res.json({ message: 'User updated' });
   } catch (err) {
     res.status(500).json({ message: err.message });
