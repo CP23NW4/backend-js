fbe80b45239f948d0fb4956d82569028979f1383
punchsppt <supapit.r@mail.kmutt.ac.th>
2024-01-13 16:47:54 +0700

    SP4-PBI8: UPDATE format code

--- src/controllers/strayAnimalController.js
+++ src/controllers/strayAnimalController.js
@@ -1,80 +1,85 @@
 // strayAnimalController.js for handling operations related to stray animals.
 
-const { validationResult } = require('express-validator');
-const StrayAnimal = require('../models/StrayAnimal');
-const azureBlobService = require('../services/azureBlobService');
-const axios = require('axios');
-require('dotenv').config({ path: '../.env' });
-const fs = require('fs');
+const { validationResult } = require('express-validator')
+const StrayAnimal = require('../models/StrayAnimal')
+const azureBlobService = require('../services/azureBlobService')
+const axios = require('axios')
+require('dotenv').config({ path: '../.env' })
+const fs = require('fs')
 
-const { uploadImageToBlob } = require('../services/azureBlobService'); // Adjust the path as needed
-
-
+const { uploadImageToBlob } = require('../services/azureBlobService') // Adjust the path as needed
 
 // Get all stray animals
 const getAllStrayAnimals = async (req, res) => {
-    try {
-      const allStrayAnimals = await StrayAnimal.find().sort({ createdOn: -1 });
-      res.json(allStrayAnimals);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
+  try {
+    const allStrayAnimals = await StrayAnimal.find().sort({ createdOn: -1 })
+    res.json(allStrayAnimals)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
 
-
-  // Get animal by ID
+// Get animal by ID
 const getStrayAnimalById = async (req, res) => {
-    try {
-      const strayAnimalbyId = await StrayAnimal.findById(req.params.saId);
-      if (!strayAnimalbyId) {
-        return res.status(404).json({ message: 'Stray animal not found' });
-      }
-      res.json(strayAnimalbyId);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+  try {
+    const strayAnimalbyId = await StrayAnimal.findById(req.params.saId)
+    if (!strayAnimalbyId) {
+      return res.status(404).json({ message: 'Stray animal not found' })
     }
-  };
+    res.json(strayAnimalbyId)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
 
-  // Create a new stray animal
+// Create a new stray animal
 const createStrayAnimal = async (req, res) => {
-    const errors = validationResult(req);
-  
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map(error => ({ errorMessages: error.msg }));
-      return res.status(400).json(errorMessages);
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors
+      .array()
+      .map((error) => ({ errorMessages: error.msg }))
+    return res.status(400).json(errorMessages)
+  }
+
+  const { name, picture, type, gender, color, description } = req.body
+
+  // Handle image upload to Azure Blob Storage
+  try {
+    let containerName
+
+    // Determine the container based on the animal type
+    if (type.toLowerCase() === 'dog') {
+      containerName = 'dogs'
+    } else if (type.toLowerCase() === 'cat') {
+      containerName = 'cats'
+    } else {
+      return res.status(400).json({
+        message: 'Invalid animal type. Supported types are Dog and Cat.',
+      })
     }
-  
-    const { name, picture, type, gender, color, description } = req.body;
 
-    // Handle image upload to Azure Blob Storage
-    try {
-      let containerName;
+    let imageUrl
 
-      // Determine the container based on the animal type
-      if (type.toLowerCase() === 'dog') {
-        containerName = 'dogs';
-      } else if (type.toLowerCase() === 'cat') {
-        containerName = 'cats';
-      } else {
-        return res.status(400).json({ message: 'Invalid animal type. Supported types are Dog and Cat.' });
-      }
+    if (isExternalUrl(picture)) {
+      // If the picture is an external URL, use it directly
+      imageUrl = picture
+    } else {
+      // If the picture is a local file path, upload it to Azure Blob Storage
+      const imageBuffer = await readFileAsync(picture)
+      const fileName = `${Date.now()}_${Math.floor(Math.random() * 1000)}.jpg` // Change the filename as per your requirements
 
-      let imageUrl;
-  
-      if (isExternalUrl(picture)) {
-        // If the picture is an external URL, use it directly
-        imageUrl = picture;
-      } else {
-        // If the picture is a local file path, upload it to Azure Blob Storage
-        const imageBuffer = await readFileAsync(picture);
-        const fileName = `${Date.now()}_${Math.floor(Math.random() * 1000)}.jpg`; // Change the filename as per your requirements
-  
-        // Upload image to Azure Blob Storage
-        await azureBlobService.uploadImageToBlob(imageBuffer, fileName, containerName);
-  
-        // Set the imageUrl as the Blob URL
-        imageUrl = `https://${process.env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${containerName}/${fileName}`;
-      }
+      // Upload image to Azure Blob Storage
+      await azureBlobService.uploadImageToBlob(
+        imageBuffer,
+        fileName,
+        containerName
+      )
+
+      // Set the imageUrl as the Blob URL
+      imageUrl = `https://${process.env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${containerName}/${fileName}`
+    }
 
     // Create a new stray animal with the Azure Blob Storage URL
     const newStrayAnimal = new StrayAnimal({
@@ -85,143 +90,140 @@ const createStrayAnimal = async (req, res) => {
       color,
       description,
       createdOn: new Date(),
-    });
+    })
 
-      // Save the stray animal to the database
-      const savedStrayAnimal = await newStrayAnimal.save();
+    // Save the stray animal to the database
+    const savedStrayAnimal = await newStrayAnimal.save()
 
-      res.status(201).json(savedStrayAnimal);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Unable to create a new stray animal' });
-    }
-  };
+    res.status(201).json(savedStrayAnimal)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Unable to create a new stray animal' })
+  }
+}
 
-  // Helper function to check if a URL is external
+// Helper function to check if a URL is external
 function isExternalUrl(url) {
-  return /^(https?:\/\/|www\.)\S+$/.test(url);
+  return /^(https?:\/\/|www\.)\S+$/.test(url)
 }
 
 // Helper function to read a file asynchronously
 function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
-    // Update a stray animal by ID
+// Update a stray animal by ID
 const updateStrayAnimal = async (req, res) => {
-    const errors = validationResult(req);
-  
-    if (!errors.isEmpty()) {
-      const specificErrors = {};
-  
-      errors.array().forEach(error => {
-        if (!specificErrors[error.param]) {
-          specificErrors[error.param] = error.msg;
-        }
-      });
-  
-      return res.status(400).json(specificErrors);
-    }
-  
-    try {
-      const existingStrayAnimal = await StrayAnimal.findById(req.params.saId);
-  
-      if (!existingStrayAnimal) {
-        return res.status(404).json({ message: 'Stray animal not found' });
-      }
+  const errors = validationResult(req)
 
-      const updatedFields = {};
-      const currentDate = new Date();
+  if (!errors.isEmpty()) {
+    const specificErrors = {}
 
-      if (req.body.name) {
-        updatedFields.name = req.body.name;
+    errors.array().forEach((error) => {
+      if (!specificErrors[error.param]) {
+        specificErrors[error.param] = error.msg
       }
-      if (req.body.picture) {
-        updatedFields.picture = req.body.picture;
-      }
-      if (req.body.type) {
-        updatedFields.type = req.body.type;
-      }
-      if (req.body.gender) {
-        updatedFields.gender = req.body.gender;
-      }
-      if (req.body.color) {
-        updatedFields.color = req.body.color;
-      }
-      if (req.body.description) {
-        updatedFields.description = req.body.description;
-      }
+    })
 
-      // If there are fields to update, add/update the 'updatedOn' field
-      if (Object.keys(updatedFields).length > 0) {
-        updatedFields.updatedOn = currentDate;
-      }
-
-      const updatedStrayAnimal = await StrayAnimal.findByIdAndUpdate(
-        req.params.saId,
-        { $set: updatedFields },
-        { new: true }
-      );
-
-      res.json(updatedStrayAnimal);
-  } catch (err) {
-    res.status(500).json({ message: 'Error updating stray animal' });
+    return res.status(400).json(specificErrors)
   }
-};
 
+  try {
+    const existingStrayAnimal = await StrayAnimal.findById(req.params.saId)
+
+    if (!existingStrayAnimal) {
+      return res.status(404).json({ message: 'Stray animal not found' })
+    }
+
+    const updatedFields = {}
+    const currentDate = new Date()
+
+    if (req.body.name) {
+      updatedFields.name = req.body.name
+    }
+    if (req.body.picture) {
+      updatedFields.picture = req.body.picture
+    }
+    if (req.body.type) {
+      updatedFields.type = req.body.type
+    }
+    if (req.body.gender) {
+      updatedFields.gender = req.body.gender
+    }
+    if (req.body.color) {
+      updatedFields.color = req.body.color
+    }
+    if (req.body.description) {
+      updatedFields.description = req.body.description
+    }
+
+    // If there are fields to update, add/update the 'updatedOn' field
+    if (Object.keys(updatedFields).length > 0) {
+      updatedFields.updatedOn = currentDate
+    }
+
+    const updatedStrayAnimal = await StrayAnimal.findByIdAndUpdate(
+      req.params.saId,
+      { $set: updatedFields },
+      { new: true }
+    )
+
+    res.json(updatedStrayAnimal)
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating stray animal' })
+  }
+}
 
 // Delete a stray animal by ID
 const deleteStrayAnimal = async (req, res) => {
-    try {
-      const deletedStrayAnimal = await StrayAnimal.findByIdAndDelete(req.params.saId);
-  
-      if (!deletedStrayAnimal) {
-        return res.status(404).json({ message: 'Stray animal not found' });
-      }
-  
-      res.json({ message: 'Stray animal deleted' });
-    } catch (err) {
-      res.status(500).json({ message: 'Error deleting stray animal' });
-    }
-  };
+  try {
+    const deletedStrayAnimal = await StrayAnimal.findByIdAndDelete(
+      req.params.saId
+    )
 
-
-
-
-    // ------------------------------------------------------------------
-    const { getImageFromBlob } = require('../services/azureBlobService');
-
-    async function getImage(req, res) {
-      try {
-        const fileName = req.params.fileName; // Assuming you pass the file name as a parameter
-        const imageData = await getImageFromBlob(fileName);
-    
-        // Set response content type as image/jpeg (or appropriate content type based on your image)
-        res.writeHead(200, {
-          'Content-Type': 'image/jpeg',
-          'Content-Length': imageData.length,
-        });
-        res.end(imageData);
-      } catch (error) {
-        console.error('Error retrieving image', error);
-        res.status(500).json({ error: 'Failed to retrieve image' });
-      }
+    if (!deletedStrayAnimal) {
+      return res.status(404).json({ message: 'Stray animal not found' })
     }
 
+    res.json({ message: 'Stray animal deleted' })
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting stray animal' })
+  }
+}
 
-  module.exports = {
-    getAllStrayAnimals,
-    getStrayAnimalById,
-    createStrayAnimal,
-    updateStrayAnimal,
-    deleteStrayAnimal,
-    // uploadImage,
-    getImage,
-  };
+// ------------------------------------------------------------------
+// const { getImageFromBlob } = require('../services/azureBlobService');
 
-  
+// async function getImage(req, res) {
+//   try {
+//     const fileName = req.params.fileName; // Assuming you pass the file name as a parameter
+//     const containerName = req.params.containerName; // Assuming you pass the container name as a parameter
+
+//     const imageData = await getImageFromBlob(fileName, containerName);
+
+//     // Set response content type as image/jpeg (or appropriate content type based on your image)
+//     res.writeHead(200, {
+//       'Content-Type': 'image/jpeg',
+//       'Content-Length': imageData.length,
+//     });
+//     res.end(imageData);
+//   } catch (error) {
+//     console.error('Error retrieving image', error);
+//     res.status(500).json({ error: 'Failed to retrieve image' });
+//   }
+// }
+
+module.exports = {
+  getAllStrayAnimals,
+  getStrayAnimalById,
+  createStrayAnimal,
+  updateStrayAnimal,
+  deleteStrayAnimal,
+  // uploadImage,
+  // getImage,
+}
--- src/services/azureBlobService.js
+++ src/services/azureBlobService.js
@@ -1,53 +1,50 @@
 // azureBlobService.js to encapsulate Azure Blob Storage functionality
 
-const { BlobServiceClient } = require('@azure/storage-blob');
-
-require('dotenv').config({ path: '../.env' });
-
-
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const { BlobServiceClient } = require('@azure/storage-blob')
+require('dotenv').config({ path: '../.env' })
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING
 
 // Remove the default containerName here
-
-const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+const blobServiceClient =
+  BlobServiceClient.fromConnectionString(connectionString)
 
 async function uploadImageToBlob(imageData, fileName, containerName) {
   // Get the container client dynamically based on the provided containerName
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  
-  const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-  const uploadBlobResponse = await blockBlobClient.uploadData(imageData);
-  return uploadBlobResponse;
+  const containerClient = blobServiceClient.getContainerClient(containerName)
+
+  const blockBlobClient = containerClient.getBlockBlobClient(fileName)
+  const uploadBlobResponse = await blockBlobClient.uploadData(imageData)
+  return uploadBlobResponse
 }
 
 // Add more functions for retrieving/deleting images as needed
 
-async function getImageFromBlob(fileName, containerName) {
-  // Get the container client dynamically based on the provided containerName
-  const containerClient = blobServiceClient.getContainerClient(containerName);
+// async function getImageFromBlob(fileName, containerName) {
+//   // Get the container client dynamically based on the provided containerName
+//   const containerClient = blobServiceClient.getContainerClient(containerName);
 
-  const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-  const downloadBlockBlobResponse = await blockBlobClient.download(0);
-  const data = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
-  return data;
+//   const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+//   const downloadBlockBlobResponse = await blockBlobClient.download(0);
+//   const data = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
+//   return data;
+// }
+
+// Helper function to convert stream to buffer
+function streamToBuffer(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = []
+    readableStream.on('data', (data) => {
+      chunks.push(data instanceof Buffer ? data : Buffer.from(data))
+    })
+    readableStream.on('end', () => {
+      resolve(Buffer.concat(chunks))
+    })
+    readableStream.on('error', reject)
+  })
 }
-
-  // Helper function to convert stream to buffer
-  function streamToBuffer(readableStream) {
-    return new Promise((resolve, reject) => {
-      const chunks = [];
-      readableStream.on('data', (data) => {
-        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-      });
-      readableStream.on('end', () => {
-        resolve(Buffer.concat(chunks));
-      });
-      readableStream.on('error', reject);
-    });
-  }
 
 module.exports = {
   uploadImageToBlob,
-  getImageFromBlob,
+  // getImageFromBlob,
   // export other functions if needed
-};
+}
