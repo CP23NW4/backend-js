4cd0f8886259c6932aeb1f73d4d86290241694b8
punchsppt <supapit.r@mail.kmutt.ac.th>
2024-02-29 16:52:30 +0700

    CLEAN: clean all code

--- src/routes/thailandRoutes.js
+++ src/routes/thailandRoutes.js
@@ -9,19 +9,3 @@ router.get('/', thailandController.getAllThailandData);
 router.get('/:thId', thailandController.getThailandDataById);
 
 module.exports = router;
-
-// const express = require('express');
-// const router = express.Router();
-// const Thailand = require('../models/Thailand');
-
-// // Route to get all Thailand data
-// router.get('/', async (req, res) => {
-//     try {
-//         const thailandData = await Thailand.find();
-//         res.json(thailandData);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// module.exports = router;
